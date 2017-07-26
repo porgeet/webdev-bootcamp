a4acb9b0ce515b913e59497320c94ceef1d2a3bf
porgeet <peter@designedbypeter.co.uk>
2017-07-26 16:57:04 +0100

    javascript comparison operators and logic

---
+++ 08_Including_Scripts/stalker.js
@@ -0,0 +1,7 @@
+var firstName	=	prompt("What is your first name? "),
+	lastName	=	prompt("What is your last name? ");
+	age			=	prompt("How old are you? "),
+	fullName	=	firstName + " " + lastName;
+
+console.log("Hello " + fullName + " how are you?");
+console.log("Oh you're " + age + " that's cool");
