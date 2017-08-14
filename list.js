5ec2d67e929eb4bbdb89cb22c2839577c91a56e1
porgeet <peter@designedbypeter.co.uk>
2017-08-14 23:34:13 +0100

    added some functions to the todo app

--- list.js
+++ list.js
@@ -4,15 +4,38 @@ var input = prompt("What would you like to do?");
 
 while(input !== "quit"){
 	if(input === "list"){
-		console.log(todos);
+		listTodos();
 	} else if (input === "new"){
-		// ask for new todo
-		var newTodo = prompt("Enter new to do");
-		// add to todos array
-		todos.push(newTodo);
+		addTodo();
+	} else if (input === "delete"){
+		deleteTodo();
 	}
 	// ask again for new input
 	var input = prompt("What would you like to do?");
-
 }
+
 console.log("oh you quit the app");
+
+function listTodos(){
+	console.log("***********");
+	todos.forEach(function(todo, index){
+		console.log(index + ": " + todo);
+	});
+	console.log("***********");
+}
+
+function addTodo(){
+	console.log("New todo added");
+	// ask for new todo
+	var newTodo = prompt("Enter new to do");
+	// add to todos array
+	todos.push(newTodo);
+}
+
+function deleteTodo(){
+	// ask for index to be deleted
+	var index = prompt("Enter the index of the todo to delete");
+	// delete that todo
+	todos.splice(index, 1);
+	console.log("Deleted todo");
+}
