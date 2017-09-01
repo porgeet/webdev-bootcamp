918c9a40ff2a096397cd74bfd5134e12a4954bbe
porgeet <peter@designedbypeter.co.uk>
2017-09-01 14:50:10 +0100

    Learned about document methods for selecting DOM elements

---
+++ selector-exercise.js
@@ -0,0 +1,9 @@
+var method1 = document.getElementById("first");
+var method2 = document.getElementsByTagName("p")[0];
+var method3 = document.querySelector(".special");
+var method4 = document.querySelectorAll(".special")[0];
+
+console.log(method1);
+console.log(method2);
+console.log(method3);
+console.log(method4);
