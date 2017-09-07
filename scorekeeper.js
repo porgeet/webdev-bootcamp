e891fa341774d69d67d3eaf1dddf2f54cdea85af
porgeet <peter@designedbypeter.co.uk>
2017-09-07 14:07:09 +0100

    further event listener learning, applying other logic to begin to create apps

---
+++ scorekeeper.js
@@ -0,0 +1,8 @@
+// selecting elements
+var p1 = document.getElementById("p1"),
+	p2 = document.querySelector("#p2"),
+	reset = document.getElementById("reset");
+
+p1.addEventListener("click", function(){
+	alert("clicked");
+});
--- toggle.js
+++ toggle.js
@@ -1,5 +1,16 @@
 var button = document.querySelector("button");
 
+// button.addEventListener("click", function(){
+// 	document.body.classList.toggle("purple");
+// });
+
+var isPurple = false;
+
 button.addEventListener("click", function(){
-	document.body.classList.toggle("purple");
+	if(isPurple){
+		document.body.style.background = "white";
+	} else {
+		document.body.style.background = "purple";
+	}
+	isPurple = !isPurple;
 });
