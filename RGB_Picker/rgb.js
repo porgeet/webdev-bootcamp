5d5977b4cebbad48caa3984e0ba6ab01fc2f454b
porgeet <peter@designedbypeter.co.uk>
2017-09-13 14:04:24 +0100

    almost done on rbg colour game app

--- RGB_Picker/rgb.js
+++ RGB_Picker/rgb.js
@@ -1,16 +1,10 @@
-var colors = [
-	"rgb(255, 0, 0)",
-	"rgb(255, 255, 0)",
-	"rgb(0, 255, 0)",
-	"rgb(0, 255, 255)",
-	"rgb(0, 0, 255)",
-	"rgb(255, 0, 255)",
+var colors = generateRandomColors(6);
 
-];
-
-var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
-var colorDisplay = document.getElementById("color-display");
+var squares = document.querySelectorAll(".square"),
+	pickedColor = pickColor(),
+	colorDisplay = document.getElementById("color-display"),
+	h1 = document.querySelector("h1"),
+	messageDisplay = document.querySelector("#message");
 
 colorDisplay.textContent = pickedColor;
 
@@ -23,10 +17,48 @@ for(i=0; i<squares.length; i++){
 		var clickedColor = this.style.backgroundColor;
 		// compare clicked color to pickedColor
 		if(clickedColor === pickedColor){
-			alert("CORRECT");
+			messageDisplay.textContent = "Correct!";
+			changeColors(clickedColor);
+			h1.style.backgroundColor = clickedColor;
 		} else {
-			alert("WRONG");
+			this.style.backgroundColor = "#232323";
+			messageDisplay.textContent = "Try again";
 		}
 	});
 }
 
+function changeColors(color){
+	// loop through all squares
+	for(i=0; i<colors.length; i++){
+		// change color to match given color
+		squares[i].style.backgroundColor = color;
+	}
+}
+
+function pickColor(){
+	var random = Math.floor(Math.random() * colors.length);
+	return colors[random];
+}
+
+function generateRandomColors(num){
+	// make and array
+	var arr = [];
+	// repeat num times
+	for(i=0; i<num; i++){
+		// get random color and puss into arr
+		arr.push(randomColor());
+	}
+	// return arr
+	return arr;
+}
+
+function randomColor(){
+	// pick red from 0 to 255
+	var r = Math.floor(Math.random() * 256),
+	// pick gree from 0 to 255
+		g = Math.floor(Math.random() * 256),
+	// pick blue from 0 to 255
+		b = Math.floor(Math.random() * 256);
+	// concatinate rgb(x, x, x) string
+	return "rgb(" + r + ", " + g + ", " + b +")"
+}
