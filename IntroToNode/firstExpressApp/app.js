e05642d1d83d093e7b89a8fe117cf3020480155e
porgeet <peter@designedbypeter.co.uk>
2017-10-18 14:07:05 +0100

    Learning about package.json, npm init and the --save flag

--- IntroToNode/firstExpressApp/app.js
+++ IntroToNode/firstExpressApp/app.js
@@ -11,6 +11,7 @@ app.get("/bye", function(req, res){
 });
 //  "/dog" => "MEOW!"
 app.get("/dog", function(req, res){
+	console.log("Someone made a request to /dog");
 	res.send("MEOW!");
 });
 //  Tell Express to listen for requests (start server)
