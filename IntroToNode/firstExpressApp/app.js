bd447d5e1f1a53252774474213a6b5122cbc6b9a
porgeet <peter@designedbypeter.co.uk>
2017-10-20 16:44:59 +0100

    learning about routing

--- IntroToNode/firstExpressApp/app.js
+++ IntroToNode/firstExpressApp/app.js
@@ -1,6 +1,10 @@
 var express = require ("express");
 var app = express();
 
+//  "*" (any route not explicitly defined) => "You're a star"
+app.get("*", function(req, res){
+	res.send("You are a star");
+});
 //  "/" => "Hi there!"
 app.get("/", function(req, res){
 	res.send("Hi there!");
@@ -14,6 +18,7 @@ app.get("/dog", function(req, res){
 	console.log("Someone made a request to /dog");
 	res.send("MEOW!");
 });
+
 //  Tell Express to listen for requests (start server)
 app.listen(3000, function(){
 	console.log("Example app listening on port 3000!");
