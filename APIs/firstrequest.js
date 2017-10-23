402a2d847dd4eb573dd86465c9f0927f4a090fa0
porgeet <peter@designedbypeter.co.uk>
2017-10-23 22:42:53 +0100

    learning how to interface with APIs~

---
+++ APIs/firstrequest.js
@@ -0,0 +1,12 @@
+var request = require('request');
+
+request('http://www.google.com', function (error, response, body) {
+	if(error){
+		console.log(error);
+	} else {
+		if(response.statusCode == 200){
+	  		// things worked!
+			console.log(body);	
+		}
+	}
+});
