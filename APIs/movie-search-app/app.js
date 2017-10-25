7837340e682edba99085bdbb24edd2fa021c82b8
porgeet <peter@designedbypeter.co.uk>
2017-10-25 23:16:24 +0100

    Using search routes to construct URL query strings for API endpoints

--- APIs/movie-search-app/app.js
+++ APIs/movie-search-app/app.js
@@ -2,13 +2,24 @@ var express = require("express"),
 	app = express(),
 	request = require("request");
 
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+	res.render("search");
+});
+
 app.get("/results", function(req, res){
-	request("http://www.omdbapi.com/?s=blair&apikey=thewdb", function(error, response, body){
+
+	var query = req.query.search;
+	var url = "http://www.omdbapi.com/?s=" + query +"&apikey=thewdb"
+	
+	request(url, function(error, response, body){
 		if(!error && response.statusCode == 200){
-			var results = JSON.parse(body);
-			res.send(results["Search"][0]["Title"]);
+			var data = JSON.parse(body); //turn string to JS object
+			res.render("results", {data: data});
 		}
 	});
+
 });
 
 app.listen(3000, function(){
