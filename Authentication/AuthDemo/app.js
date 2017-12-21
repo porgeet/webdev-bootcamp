da28df4672a4e4d750e0a6444361710dc90d0739
porgeet <peter@designedbypeter.co.uk>
2017-12-21 07:50:07 +0000

    restarting auth section as it's hard to understand

--- Authentication/AuthDemo/app.js
+++ Authentication/AuthDemo/app.js
@@ -1,34 +1,28 @@
-var passportLocalMongoose	= require("passport-local-mongoose"),
-	LocalStrategy			= require("passport-local"),
+var PassportLocalMongoose	= require("passport-local-mongoose"),
+	LocalStrategy 			= require("passport-local"),
 	bodyParser				= require("body-parser"),
 	mongoose				= require("mongoose"),
-	passport				= require("passport")
-	express 				= require("express"),
+	passport				= require("passport"),
+	session					= require("express-session"),
+	express					= require("express"),
 	User					= require("./models/user"),
 	app						= express();
-	
-mongoose.connect("mongodb://localhost/auth_demo", {useMongoClient: true});
 
-app.use(require("express-session")({
-	secret: "Purple cats are not a common occurrence",
-	resave :false,
-	saveUninitialized: false
-}));
+mongoose.connect("mongodb://localhost", {useMongoClient: true});
 
 app.set("view engine", "ejs");
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(session({
+	secret: "Super poo poo pants up in here", // encryption key, should be stored in ENV var. NEVER pushed to repo
+	resave: false,
+	saveUninitialized: false
+}));
 
-passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser);
+passport.deserializeUser(User.deserializeUser());
 
-// =============
-// ROUTES
-// =============
-
-app.get("/", function(req, res){
+app.get("/", function(req,res){
 	res.render("home");
 });
 
@@ -36,41 +30,6 @@ app.get("/secret", function(req, res){
 	res.render("secret");
 });
 
-// Auth Routes
-
-// show sign up form
-app.get("/register", function(req, res){
-	res.render("register");
-});
-
-// handle user sign up
-app.post("/register", function(req, res){
-	req.body.username
-	req.body.password
-	User.register(new User({username: req.body.username}), req.body.password, function(err, user){
-		if(err){
-			console.log(err);
-			return res.render("register");
-		}
-		passport.authenticate("local")(req, res, function(){
-			res.redirect("/secret");
-		});
-	});
-});
-
-// LOGIN ROUTES
-
-// Render login form
-app.get("/login", function(req, res){
-	res.render("login");
-});
-
-app.post("/login", passport.authenticate("local", {
-	successRedirect: "/secret",
-	failureRedirect: "/login"
-}), function(req, res){
-});
-
-app.listen(3000, function() {
+app.listen(3000, function(){
 	console.log("Server started");
 });
